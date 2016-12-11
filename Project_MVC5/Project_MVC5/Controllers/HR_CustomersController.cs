7118021ad6f5abca8bbd54a727d21ebd87ecc3ca
Chathuranga <creedsadun@live.com>
2016-12-11 16:01:45 +0530

    Added new js files

---
+++ Project_MVC5/Project_MVC5/Controllers/HR_CustomersController.cs
@@ -0,0 +1,20 @@
+﻿using Project_MVC5.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Project_MVC5.Controllers
+{
+    public class HR_CustomersController : Controller
+    {
+        Demo_onlineEntities db = new Demo_onlineEntities();
+        // GET: HR_Customers
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
