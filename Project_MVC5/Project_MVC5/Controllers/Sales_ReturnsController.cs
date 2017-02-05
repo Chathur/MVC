cbc5ae8cd25e326f3cf7041680ac067b7d25152d
Chathuranga <creedsadun@live.com>
2017-02-05 18:55:43 +0530

    Sales Returns is now working

--- Project_MVC5/Project_MVC5/Controllers/Sales_ReturnsController.cs
+++ Project_MVC5/Project_MVC5/Controllers/Sales_ReturnsController.cs
@@ -1,5 +1,6 @@
 ﻿using Project_MVC5.Models;
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -10,9 +11,12 @@ namespace Project_MVC5.Controllers
     public class Sales_ReturnsController : Controller
     {
         WICKRAMA_STORESEntities db = new WICKRAMA_STORESEntities();
+       
+
         // GET: Sales_Return
         public ActionResult Index()
         {
+
             ViewBag.products = new SelectList(db.ITEMS, "Name_Item", "Name_Item");
             return View();
         }
@@ -22,8 +26,10 @@ namespace Project_MVC5.Controllers
             toDB.Bill_No = model.Bill_No;
             toDB.Name_Item = model.Name_Item;
             toDB.Quantity = model.Quantity;
+            toDB.Date_Returned = model.Date_Returned;
 
             db.tb_SalesReturns.Add(toDB);
+            db.SaveChanges();
 
             return RedirectToAction("Index", "Sales_Returns");
         }
